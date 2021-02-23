8f0b924e15cae927db8b61e258ac06dddf84ad02
Erick - <erick.augustoramos@gmail.com>
2021-02-22 21:03:04 -0300

    refactor in the project

---
+++ nlw2/moveit/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import { Bar } from './components/Bar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Bar />
+    </div>
+  );
+}
+
+export default App;
---
+++ nlw2/moveit/src/components/Bar/index.tsx
@@ -0,0 +1,12 @@
+export function Bar() {
+  return (
+    <header className="bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '50%' }} />
+        <span className="cirrent" style={{ left: '50%' }}>300xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+};
