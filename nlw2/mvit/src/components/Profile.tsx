17349e0d72fbb06f1ba26726c768ab965f09b1c9
Erick - <erick.augustoramos@gmail.com>
2021-02-25 19:37:38 -0300

    add changes to the context

--- nlw2/mvit/src/components/Profile.tsx
+++ nlw2/mvit/src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/erickaugustor.png" alt="Erick" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Erick</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Nível 1
+          {`Nível ${level}`}
         </p>
       </div>
     </div>
