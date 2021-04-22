cc0e65116eaf33e816b527deb6aa971cbe549a52
Erick - <erick.augustoramos@gmail.com>
2021-04-21 22:22:28 -0300

    start implement changes

--- nlw3/next-podcast/src/pages/index.tsx
+++ nlw3/next-podcast/src/pages/index.tsx
@@ -1,6 +1,24 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
-export default function Home(props) {
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt'
+
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+
+type HomeProps = {
+  episodeos: Array<{
+    id: string
+    thumbmail: string
+    title: string
+    members: string
+    publish_at: string,
+    publishAt: string,
+  }>
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <Head>
@@ -8,6 +26,9 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div>
+        
+      </div>
       Oie
     </div>
   )
@@ -16,13 +37,25 @@ export default function Home(props) {
 // getServerSideProps everytime
 // getStaticSideProps once
 
-export async function getStaticSideProps() {
-  const response = await fetch('http://localhost:3333/episodeos');
-  const data = await response.json();
+export const getStaticSideProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodeos', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodeos = data.map(episodeo => ({
+    ...episodeo,
+    pulishedAt: format(parseISO(episodeo.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(episodeo.duration),
+    durationAsString: convertDurationToTimeString(Number(episodeo.file.duration))
+  }))
 
   return {
     props: {
-      episodeos: data,
+      episodeos,
     },
     revalidate: 60 * 60 * 8,
   }
